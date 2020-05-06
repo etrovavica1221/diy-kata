633651601549d673c022e896f88632bc8c66499a
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-05-06 21:16:34 +0100

    Add tests for fizzBuzz,booleanToWord,numberToReversedDigits

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,9 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+  const str = number.toString();
+  const spl = str.split('');
+  const rev = spl.reverse();
+  const arr = rev.map(ar => Number(ar));
+  return arr;
+};
 
 module.exports = numberToReversedDigits;
