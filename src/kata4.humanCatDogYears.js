14bbb18ea77138765c558e5d8bf2fe4516e8513a
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-05-07 11:18:34 +0100

    Add a test for humanCatDogYears kata

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,16 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+  const arr = [];
+  if (number === 1) {
+    arr.push(1, 15, 15);
+  } else if (number === 2) {
+    arr.push(2, 24, 24);
+  } else {
+    const cat = (number - 2) * 4 + 24;
+    const dog = (number - 2) * 5 + 24;
+    arr.push(number, cat, dog);
+  }
+
+  return arr;
+};
 
 module.exports = humanCatDogYears;
