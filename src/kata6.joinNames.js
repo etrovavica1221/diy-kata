40c845218840dcedbac852f28af8104371a7d1c1
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-05-07 23:16:23 +0100

    Add tests for Kata6.joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,22 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+  const namesArr = namesObj.map(obj => obj.name.toString());
+  namesArr.pop();
+  let namesString = "";
+
+  for (let i = 0; i < namesArr.length; i++) {
+    if (i === 0) {
+      namesString += `${namesArr[i]},`;
+      if (i === namesArr.length - 1) {
+        namesString = namesArr[i];
+      }
+    } else if (i === namesArr.length - 1) {
+      namesString += " " + namesArr[i];
+    } else {
+      namesString += ` ${namesArr[i]},`;
+    }
+  };
+  const lastName = namesObj.pop().name;
+  return `${namesString} & ${lastName}`;
+};
 
 module.exports = joinNames;
