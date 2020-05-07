b754e010728f1c028ed69faa3dc635e8bdf6be34
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-05-07 11:21:22 +0100

    Add a test for humanCatDogYears kata

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,10 @@
 const { humanCatDogYears } = require("../src");
 
+test("returns array of human, cat and dog years when passed human years", () => {
+  expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+  expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+  expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+  expect(humanCatDogYears(100)).toEqual([100, 416, 514]);
+});
+
 // Look Ma, no handlebars!!!
