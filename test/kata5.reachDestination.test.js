709e2b408936d90d877db1f7ed62d6d80bab6cc8
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-05-07 11:58:10 +0100

    Add tests for reachDestination kata

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44,10)).toBe("I should be there in 4.5 hours.");
+    expect(reachDestination(100,50)).toBe("I should be there in 2 hours.");
+    expect(reachDestination(74,41)).toBe("I should be there in 2 hours.");
+  });
 });
